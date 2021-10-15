0d64c5ca9ea34b0984fef252849c5165bce15b61
Lucas Gomes <lucasg@lacunasoftware.com>
2021-10-15 02:34:51 -0300

    Refactored code to express and dockerized project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(port, function () {
+  console.log(`App listening on port ${port}.`);
+});
